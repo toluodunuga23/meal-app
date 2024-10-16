ede923503958a164306116407836011214d84c36
Tolu Odunuga <to379@drexel.edu>
2024-10-15 23:55:06 -0400

    hero redesign

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -3,19 +3,41 @@ import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen py-16 gap-6 bg-gradient-to-r from-blue-500 to-purple-500 text-white">
-      <h1 className="font-extrabold text-[60px] leading-tight text-center">
-        The Ultimate Food App with <span className="text-yellow-400">MealBuddy</span>
-      </h1>
-      <p className="text-lg text-center max-w-xl">
-        Your one-stop shop for meal planning, grocery shopping, and recipe management and meal spots near you.
-      </p>
-      <Link to ={'/create-meal'}>
-      <Button className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-3 px-6 rounded-full">
-        Get Started
-      </Button>
-      </Link>
-    </div>
+    <>
+
+      <div className="bg-[url('/food.svg')] min-h-screen bg-cover bg-no-repeat flex flex-col items-center justify-center  ">
+
+        <div className="px-10 pt-80 mb-40  flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
+            The Ultimate Food App with <span className="text-yellow-500">MealBuddy</span>
+          </h1>
+          <p className="text-white text mt-5 ml-20 font-semibold">
+            Your one-stop shop for meal planning, grocery shopping, and recipe management and meal spots near you.
+          </p>
+
+          <Link to={'/create-meal'}>
+            <Button className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-3 px-6 rounded-full mt-8   ">
+              Get Started
+            </Button>
+          </Link>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+
+    </>
   )
 }
 
